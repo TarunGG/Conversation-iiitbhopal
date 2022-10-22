04e9da9cf0a9f4c320aa451116e7ab17b97ff9a9
Tarun Garg <96339593+Evil2004@users.noreply.github.com>
2022-10-22 12:55:13 +0530

    project

--- profile.go
+++ profile.go
@@ -14,21 +14,21 @@ func profile(w http.ResponseWriter, r *http.Request) {
 		split := strings.Split(cookie.Value, "|")
 		username := split[1]
 
-		query := `SELECT UserName, Name, Email FROM users WHERE UserName=` + `"` + username + `"`
+		query := `SELECT username, name, email FROM users WHERE username=` + `'` + username + `'`
 		rows, err := db.Query(query)
 		checkerr(err)
 		for rows.Next() {
 			rows.Scan(&profile.UserName, &profile.Name, &profile.Email)
 		}
 		fmt.Println(profile.UserName, profile.Email, profile.Name)
-		query = `SELECT * FROM thread WHERE UserName=` + `"` + username + `"`
+		query = `SELECT * FROM thread WHERE username=` + `"` + username + `"`
 
 		rows, err = db.Query(query)
 		checkerr(err)
 		for rows.Next() {
 			profile.NoThread++
 		}
-		query = `SELECT * FROM post WHERE PostUserName=` + `"` + username + `"`
+		query = `SELECT * FROM post WHERE post_user_name=` + `"` + username + `"`
 
 		rows, err = db.Query(query)
 		checkerr(err)
